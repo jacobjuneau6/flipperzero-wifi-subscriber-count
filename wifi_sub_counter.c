5667a2c2bad8a49154505bdcc3c31c453757afd8
jacobjuneau6 <79861515+jacobjuneau6@users.noreply.github.com>
2023-07-07 12:00:41 +0000

    fix

--- wifi_sub_counter.c
+++ wifi_sub_counter.c
@@ -4,8 +4,8 @@
 #include <gui/gui.h>
 #include <input/input.h>
 
+
 int uart_fd;
-int selected_baud_rate = 115200; // Default baud rate
 
 void uart_init() {
     uart_fd = flipper_open("/dev/ttyS0", O_RDWR); // Update with your actual serial port
@@ -13,7 +13,7 @@ void uart_init() {
         printf("Failed to open UART\n");
         return;
     }
-    flipper_configure(uart_fd, FLIPPER_UART_MODE, selected_baud_rate);
+    flipper_configure(uart_fd, FLIPPER_UART_MODE, 115200); // Update with your actual baudrate
 }
 
 void uart_send_command(const char* command) {
@@ -23,56 +23,10 @@ void uart_send_command(const char* command) {
     printf("Response: %s\n", response);
 }
 
-void menu_select_baud_rate() {
-    printf("Select Baud Rate:\n");
-    printf("1. 9600\n");
-    printf("2. 115200 (default)\n");
-    printf("3. 57600\n");
-    printf("4. 38400\n");
-
-    int option;
-    printf("Enter option: ");
-    scanf("%d", &option);
-
-    switch (option) {
-        case 1:
-            selected_baud_rate = 9600;
-            break;
-        case 2:
-            selected_baud_rate = 115200;
-            break;
-        case 3:
-            selected_baud_rate = 57600;
-            break;
-        case 4:
-            selected_baud_rate = 38400;
-            break;
-        default:
-            printf("Invalid option\n");
-            return;
-    }
-
-    flipper_configure(uart_fd, FLIPPER_UART_MODE, selected_baud_rate);
-    printf("Baud rate set to %d\n", selected_baud_rate);
-}
-
-void on_button_press(button_t button) {
-    switch (button) {
-        case BUTTON_CENTER:
-            uart_send_command("YOUR_UART_COMMAND\r\n"); // Update with your actual UART command
-            break;
-        case BUTTON_RIGHT:
-            menu_select_baud_rate();
-            break;
-        default:
-            break;
-    }
-}
-
 int main() {
     lf_attach(0);
     uart_init();
-    flipper_button_subscribe(BUTTON_CENTER | BUTTON_RIGHT, 0, on_button_press);
+    flipper_button_subscribe(BUTTON_CENTER, 0, uart_send_command);
 
     while (1) {
         lf_spin();
